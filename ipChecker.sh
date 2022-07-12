74aef52fe160beea0a35e4f96d0597cfaf1ac6c6
Alexandre <alexandre@qwest.tv>
2022-07-12 14:21:15 +0200

    Added script and history file

---
+++ ipChecker.sh
@@ -0,0 +1,33 @@
+## Moving the current working directory for local sources
+parent_path=$(
+  # shellcheck disable=SC2164
+  cd "$(dirname "${BASH_SOURCE[0]}")"
+  pwd -P
+)
+# shellcheck disable=SC2164
+cd "$parent_path"
+
+ipFile="./ipHistory"
+mailToSend="example@mail.com"
+
+lastKnownIP=$(tail -1 "${ipFile}")
+echo "${lastKnownIP}"
+NOW=$(date +"%d-%m-%YT%T")
+echo "${NOW}"
+
+actualIP=$(dig +short myip.opendns.com @resolver1.opendns.com)
+echo "${actualIP}"
+
+if [ "${lastKnownIP}" = "${actualIP}" ]; then
+  echo "Same IP"
+else
+  echo "Different IP"
+  printf "Subject: Device IP changed\n
+New IP : %s
+Change it at https://domains.google.com/
+
+This is an automated message, please don't reply.
+  " "${actualIP}" | ssmtp "${mailToSend}"
+  echo "${NOW}" >> "${ipFile}"
+  echo "${actualIP}" >> "${ipFile}"
+fi
